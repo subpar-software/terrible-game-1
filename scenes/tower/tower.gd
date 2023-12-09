ebfbe7f0c588eafba6a75960483df32e6883a637
James <james@geekwagon.net>
2023-12-08 19:14:07 -0600

    Looks like I was doing some refactoring

--- scenes/tower/tower.gd
+++ scenes/tower/tower.gd
@@ -1,17 +1,16 @@
 extends Node2D
 
-signal tower_selected
-signal tower_deselected
+signal selected
 
 @export var controller: String = "None"
 @export var level: int = 1
 var controller_color
 var controller_selected_color
 
-var selected: bool = false
+var is_selected: bool = false
 
 var max_dudes = 1
-var attacking = []
+var sending_dudes_to = []
 
 var attackable_towers = []
 
@@ -22,7 +21,7 @@ var dude = preload("res://scenes/dude/dude.tscn")
 
 func _ready():
 	var input_handler = get_tree().get_root().get_node("InputHandler")
-	input_handler.yo_getting_attacked.connect(_on_getting_attacked)
+	input_handler.send_dudes.connect(_on_sending_dudes)
 	
 	if (controller == "Enemy"):
 		enemy_setup()
@@ -39,9 +38,10 @@ func enemy_setup():
 func _process(_delta):
 	controller_color = ControllerColors.get_color[controller]
 	controller_selected_color = ControllerColors.get_selected_color[controller]
-	$Sprite2D.modulate = controller_selected_color if selected else controller_color
+	$Sprite2D.modulate = controller_selected_color if is_selected else controller_color
 
 	$LevelLabel.text = str(level)
+	$SendingLabel.text = str(sending_dudes_to.size())
 	
 	queue_redraw()
 
@@ -50,49 +50,43 @@ func _process(_delta):
 	if level >= 20:
 		max_dudes = 3
 
-	if enemy_take_action && controller == "Enemy" && enemy_decision_counter % 10 == 0 && attackable_towers.size() > 0:
+	if enemy_take_action && controller == "Enemy" && enemy_decision_counter % 2 == 0 && attackable_towers.size() > 0:
 		enemy_take_action = false
-		var last_tower = attackable_towers.pop_back()
-		attacking.append(last_tower)
+		if len(sending_dudes_to) < max_dudes:
+			var last_tower = attackable_towers.pop_back()
+			sending_dudes_to.append(last_tower)
 
 
 func _on_area_2d_input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-			# print("Left button was clicked at ", event.position)
-			selected = true
-			tower_selected.emit(self)
+			selected.emit(self)
 		if event.button_index == MOUSE_BUTTON_RIGHT and event.pressed:
-			selected = false
-			tower_deselected.emit(self)
-			attacking = []
+			sending_dudes_to = []
 
-# This should probably be in the input handler, not on each tower
-# I think that's why some towers are hard to click
+
 func _unhandled_input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-			selected = false
-			tower_deselected.emit(self)
+			is_selected = false
 
-
-func _on_getting_attacked(tower_attacking, tower_under_attack):
-	if self == tower_attacking:
-		if len(attacking) < max_dudes:
-			attacking.append(tower_under_attack)
+func _on_sending_dudes(sending_tower, recieving_tower):
+	if self == sending_tower:
+		if len(sending_dudes_to) < max_dudes:
+			sending_dudes_to.append(recieving_tower)
 
 
 func _draw():
 	var tower_center = Vector2(0, 0)
 	var line_width = 10.0
 
-	for tower in attacking:
+	for tower in sending_dudes_to:
 		var attack_tower_center = tower.global_position - global_position
 		draw_line(tower_center, attack_tower_center, controller_color, line_width)
 
 
 func _on_timer_timeout():
-	for tower in attacking:
+	for tower in sending_dudes_to:
 		var new_dude = dude.instantiate()
 		new_dude.setup(self, tower)
 		add_child(new_dude)
@@ -114,7 +108,7 @@ func _on_area_2d_area_entered(area):
 		level -= 1
 		if level == 0:
 			controller = a_dude.controller
-			attacking = []
+			sending_dudes_to = []
 			if controller == "Enemy":
 				enemy_setup()
 
--- scripts/input_handler.gd
+++ scripts/input_handler.gd
@@ -1,29 +1,56 @@
 extends Node
 
-signal yo_getting_attacked
+signal send_dudes
 
 var towers: Array[Node]
 
-var player_selected
-var other_selected
+var first_selected
+var second_selected
 
+var label
+var label2
 
 func _ready():
 	towers = get_node("../GameScene/Towers").get_children()
 	for tower in towers:
-		tower.tower_selected.connect(_on_tower_tower_selected)
+		tower.selected.connect(_on_tower_selected)
+	
+	label = get_node("../GameScene/Info/Label")
+	label2 = get_node("../GameScene/Info/Label2")
+
+func _process(_delta):
+	if first_selected:
+		label.text = first_selected.name
+	if second_selected:
+		label2.text = second_selected.name
 
 
-func _on_tower_tower_selected(tower):
-	if tower.controller == "Player" && player_selected == null:
-		player_selected = tower
+func _on_tower_selected(tower):
+	deslect_all_towers()
+	tower.is_selected = true
+	
+	if first_selected == null:
+		first_selected = tower
 		return
 	
-	if player_selected != null:
-		other_selected = tower
-		
-		if player_selected != null:
-			yo_getting_attacked.emit(player_selected, other_selected)
+	if second_selected == null && first_selected.controller == "Player":
+		second_selected = tower
+		send_dudes.emit(first_selected, second_selected)
+		first_selected = null
+		second_selected = null
 
-			player_selected = null
-			other_selected = null
+
+#	if tower.controller == "Player" && player_selected == null:
+#		player_selected = tower
+#		return
+#
+#	if player_selected != null:
+#		other_selected = tower
+#
+#		send_dudes.emit(player_selected, other_selected)
+#		player_selected = null
+#		#other_selected = null
+
+func deslect_all_towers():
+	for tower in towers:
+		tower.is_selected = false
